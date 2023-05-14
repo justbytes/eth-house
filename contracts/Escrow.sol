e4774ae0fdbb8b4fe9269ffda0765b625e649453
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-05-13 19:56:02 -0700

    added RealEstate and Escrow sol files with starter code, added tests for contracts

---
+++ contracts/Escrow.sol
@@ -0,0 +1,30 @@
+// contracts/RealEstate.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        address _id
+    ) external;
+}
+
+contract Escrow {
+    address public lender;
+    address public inspector;
+    address payable public seller;
+    address public nftAddress;
+
+    constructor(
+        address _lender, 
+        address _inspector, 
+        address payable _seller, 
+        address _nftAddress
+    ) {
+        lender = _lender;
+        inspector = _inspector;
+        seller = _seller;
+        nftAddress = _nftAddress;
+    }
+}
